af2cc874062882533818fd0394ba310b842b08e6
alx <37true@gmail.com>
2019-02-02 19:10:34 +0700

    helper addes

--- app/src/main/java/tk/alltrue/testsqlite/EditorActivity.java
+++ app/src/main/java/tk/alltrue/testsqlite/EditorActivity.java
@@ -1,14 +1,23 @@
 package tk.alltrue.testsqlite;
 
+import android.content.ContentValues;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.text.TextUtils;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.EditText;
 import android.widget.Spinner;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.app.NavUtils;
+import tk.alltrue.data.HotelContract.GuestEntry;
+import tk.alltrue.data.HotelDbHelper;
 
 public class EditorActivity extends AppCompatActivity {
 
@@ -25,6 +34,9 @@ public class EditorActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_editor);
+
+        Toolbar toolbar1 = (Toolbar) findViewById(R.id.toolbar1);
+        setSupportActionBar(toolbar1);
 
         mNameEditText = (EditText) findViewById(R.id.edit_guest_name);
         mCityEditText = (EditText) findViewById(R.id.edit_guest_city);
@@ -61,6 +73,64 @@ public class EditorActivity extends AppCompatActivity {
                 mGender = 2;
             }
         });
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_editor, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        switch (id) {
+            case R.id.action_save:
+                if(item.isChecked()==true) {
+                    insertGuest();
+                    finish();
+                    return true;}
+            case R.id.action_delete:
+                return true;
+
+            case android.R.id.home:
+                NavUtils.navigateUpFromSameTask(this);
+                return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void insertGuest() {
+        String name = mNameEditText.getText().toString().trim();
+        String city = mCityEditText.getText().toString().trim();
+        String ageString = mAgeEditText.getText().toString().trim();
+        int age = Integer.parseInt(ageString);
+
+        HotelDbHelper mDbHelper = new HotelDbHelper(this);
+
+        SQLiteDatabase db = mDbHelper.getWritableDatabase();
+
+        ContentValues values = new ContentValues();
+        values.put(GuestEntry.COLUMN_NAME, name);
+        values.put(GuestEntry.COLUMN_CITY, city);
+        values.put(GuestEntry.COLUMN_GENDER, mGender);
+        values.put(GuestEntry.COLUMN_AGE, age);
+
+        long newRowId = db.insert(GuestEntry.TABLE_NAME, null, values);
+
+        if (newRowId == -1) {
+            Toast.makeText(this, "Error to write row to DB", Toast.LENGTH_SHORT).show();
+        } else {
+            Toast.makeText(this, "Guest added to DB by row number: " + newRowId, Toast.LENGTH_SHORT).show();
+        }
     }
 
 }
