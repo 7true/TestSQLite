704d7c962acbae293607f030eb775526fcd5c3c7
alx <37true@gmail.com>
2019-02-05 03:31:41 +0700

    helper added

--- app/src/main/java/tk/alltrue/testsqlite/EditorActivity.java
+++ app/src/main/java/tk/alltrue/testsqlite/EditorActivity.java
@@ -13,6 +13,8 @@ import android.widget.EditText;
 import android.widget.Spinner;
 import android.widget.Toast;
 
+import java.sql.RowId;
+
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import androidx.core.app.NavUtils;
@@ -88,11 +90,18 @@ public class EditorActivity extends AppCompatActivity {
 
         //noinspection SimplifiableIfStatement
         switch (item.getItemId()) {
-            case R.id.action_delete:
+            case R.id.action_save:
                     insertGuest();
                     finish();
                     return true;
-
+            case R.id.action_update:
+                updateGuest();
+                finish();
+                return true;
+            case R.id.action_delete:
+                deleteGuest();
+                finish();
+                return true;
             case android.R.id.home:
                 NavUtils.navigateUpFromSameTask(this);
                 return true;
@@ -100,6 +109,7 @@ public class EditorActivity extends AppCompatActivity {
 
         return super.onOptionsItemSelected(item);
     }
+
 
     private void insertGuest() {
         String name = mNameEditText.getText().toString().trim();
@@ -124,6 +134,49 @@ public class EditorActivity extends AppCompatActivity {
         } else {
             Toast.makeText(this, "Guest added to DB by row number: " + newRowId, Toast.LENGTH_SHORT).show();
         }
+    }
+
+    private void updateGuest() {
+        String name = mNameEditText.getText().toString().trim();
+        String city = mCityEditText.getText().toString().trim();
+        String ageString = mAgeEditText.getText().toString().trim();
+
+        int age = Integer.parseInt(ageString);
+
+        HotelDbHelper mDbHelper = new HotelDbHelper(this);
+        SQLiteDatabase db = mDbHelper.getWritableDatabase();
+
+        ContentValues values = new ContentValues();
+        values.put(GuestEntry.COLUMN_NAME, name);
+        values.put(GuestEntry.COLUMN_CITY, city);
+        values.put(GuestEntry.COLUMN_GENDER, mGender);
+        values.put(GuestEntry.COLUMN_AGE, age);
+        String[] whereArgs = new String[] {String.valueOf(name)};
+        long rowId = db.update(GuestEntry.TABLE_NAME, values,GuestEntry.COLUMN_NAME + "=?",whereArgs);
+
+        if (rowId == -1) {
+            Toast.makeText(this, "Error to write row to DB", Toast.LENGTH_SHORT).show();
+        } else {
+            Toast.makeText(this, "Guest updated in DB by row number: " + rowId, Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    private void deleteGuest() {
+        String name = mNameEditText.getText().toString().trim();
+
+        HotelDbHelper mDbHelper = new HotelDbHelper(this);
+        SQLiteDatabase db = mDbHelper.getWritableDatabase();
+
+        String[] whereArgs = new String[] {String.valueOf(name)};
+
+        long rowId = db.delete(GuestEntry.TABLE_NAME, GuestEntry.COLUMN_NAME + "=?", whereArgs);
+
+        if (rowId == -1) {
+            Toast.makeText(this, "Error to write row to DB", Toast.LENGTH_SHORT).show();
+        } else {
+            Toast.makeText(this, "Guest deleted from DB by row number: " + rowId, Toast.LENGTH_SHORT).show();
+        }
+
     }
 
 }
